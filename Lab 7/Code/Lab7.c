5eaf706ddb8020f43113b2d701a8f6df29a201f5
Kyle Jonas <C16Kyle.Jonas@usafa.edu>
2014-12-06 16:07:31 -0700

    Required Functionality

--- Lab 7/Code/Lab7.c
+++ Lab 7/Code/Lab7.c
@@ -1,116 +1,94 @@
 //-----------------------------------------------------------------
-// Name:	Kyle Jonas
-// File:	lab7.c
-// Date:	Fall 2014
-// Purp:
+// Name:    Coulston
+// File:    lab7.c
+// Date:    Fall 2014
+// Purp:    Collect analog samples from P1.3 and P1.4
 //-----------------------------------------------------------------
 #include "msp430g2553.h"
 
 void initMSP430();
-
+void Right_Sensor();
+void Left_Sensor();
+void Center_Sensor();
 //----------------------------------------------------------------------
 //----------------------------------------------------------------------
 int main(void) {
 
-    // P1.1 Left
-    // P1.2 Center
-    // P1.3 Right
+    IFG1=0;                                                     // clear interrupt flag1
+    WDTCTL = WDTPW + WDTHOLD;                                   // disable WDT
 
-	unsigned short L[16];									    // To analyze the values
-//	unsigned short C[16];
-	int test;
-	unsigned short R[16];
-	unsigned char i = 0;										// index into sample array
+    BCSCTL1 = CALBC1_8MHZ;                                      // 8MHz clock
+    DCOCTL = CALDCO_8MHZ;
 
-	IFG1=0; 													// clear interrupt flag1
-	WDTCTL = WDTPW + WDTHOLD;									// disable WDT
+    P1DIR = BIT0|BIT6;                                               // Set the red LED as output
 
-	BCSCTL1 = CALBC1_8MHZ;										// 8MHz clock
-	DCOCTL = CALDCO_8MHZ;
+    P1OUT = BIT0|BIT6;
+    P1OUT &= ~(BIT0|BIT6);
 
-	P1DIR = BIT0;												// Set the red LED as output
+    while(1) {
 
-//	_enable_interrupt();
+        Left_Sensor();
+//        Center_Sensor();
+        Right_Sensor();
 
-	while(1) {
-	    // Configure P1.4 to be the ADC input
-	    ADC10CTL0 = 0;                                          // Turn off ADC subsystem
-	    ADC10CTL1 = INCH_4 | ADC10DIV_3 ;                       // Channel 4, ADC10CLK/4
-	    ADC10AE0 = BIT2;                                        // Make P1.2 analog input
-	    ADC10CTL0 = SREF_0 | ADC10SHT_3 | ADC10ON | ENC;        // Vcc & Vss as reference
-
-	    ADC10CTL0 |= ADC10SC;                                   // Start a conversion
-	    while(ADC10CTL1 & ADC10BUSY);                           // Wait for conversion to complete
-//	    C[i] = ADC10MEM;                                   // collect that 10-bit value
-	    test = ADC10MEM;
-
-	    if (test > 0x0041) P1OUT |= BIT0;                       // turn on LED when closer than 5 in
-	    else               P1OUT &= ~BIT0;                      // turn off LED
-
-	    i = (i+1) & 0xF;                            // This is a mod 16 increment
-	}
+    } // end infinite loop
 
 } // end main
 
-//#pragma vector = PORT2_VECTOR				// This is from the MSP430G2553.h file
-//__interrupt void Port_2_ISR(void) {
-//
-//
-//
-//	switch ( Sensor ) {
-//	case Left:
-//	    // Configure P1.4 to be the ADC input
-//	           ADC10CTL0 = 0;                                          // Turn off ADC subsystem
-//	           ADC10CTL1 = INCH_4 | ADC10DIV_3 ;                       // Channel 4, ADC10CLK/4
-//	           ADC10AE0 = BIT1;                                        // Make P1.1 analog input
-//	           ADC10CTL0 = SREF_0 | ADC10SHT_3 | ADC10ON | ENC;        // Vcc & Vss as reference
-//
-//	           ADC10CTL0 |= ADC10SC;                                   // Start a conversion
-//	           while(ADC10CTL1 & ADC10BUSY);                           // Wait for conversion to complete
-//	           L[i] = ADC10MEM;                                   // collect that 10-bit value
-//
-//	           if (L[i] > 0x0200) P1OUT |= BIT0;                       // turn on LED
-//               else               P1OUT &= ~BIT0;                      // turn off LED
-//
-//	           i = (i+1) & 0xF;                            // This is a mod 16 increment
-//		Sensor = Center;
-//	  break;
-//
-//	case Center:
-//	    // Configure P1.4 to be the ADC input
-//	           ADC10CTL0 = 0;                                          // Turn off ADC subsystem
-//	           ADC10CTL1 = INCH_4 | ADC10DIV_3 ;                       // Channel 4, ADC10CLK/4
-//	           ADC10AE0 = BIT2;                                        // Make P1.2 analog input
-//	           ADC10CTL0 = SREF_0 | ADC10SHT_3 | ADC10ON | ENC;        // Vcc & Vss as reference
-//
-//	           ADC10CTL0 |= ADC10SC;                                   // Start a conversion
-//	           while(ADC10CTL1 & ADC10BUSY);                           // Wait for conversion to complete
-//	           C[i] = ADC10MEM;                                   // collect that 10-bit value
-//
-//	           if (C[i] > 0x0041) P1OUT |= BIT0;                       // turn on LED when closer than 5 in
-//	           else               P1OUT &= ~BIT0;                      // turn off LED
-//
-//	           i = (i+1) & 0xF;                            // This is a mod 16 increment
-//		Sensor = Right;
-//	  break;
-//	case Right:
-//	    // Configure P1.4 to be the ADC input
-//	           ADC10CTL0 = 0;                                          // Turn off ADC subsystem
-//	           ADC10CTL1 = INCH_4 | ADC10DIV_3 ;                       // Channel 4, ADC10CLK/4
-//	           ADC10AE0 = BIT3;                                        // Make P1.3 analog input
-//	           ADC10CTL0 = SREF_0 | ADC10SHT_3 | ADC10ON | ENC;        // Vcc & Vss as reference
-//
-//	           ADC10CTL0 |= ADC10SC;                                   // Start a conversion
-//	           while(ADC10CTL1 & ADC10BUSY);                           // Wait for conversion to complete
-//	           R[i] = ADC10MEM;                                   // collect that 10-bit value
-//
-//	           if (R[i] > 0x0132) P1OUT |= BIT0;                       // turn on LED when closer than 5 in
-//               else               P1OUT &= ~BIT0;                      // turn off LED
-//
-//	           i = (i+1) & 0xF;                            // This is a mod 16 increment
-//	  break;
-//	}
-//}//end interupt
+void Left_Sensor() {
+    // LEFT
+    static unsigned short L_val[16];
+    static unsigned char x = 0;
 
+            ADC10CTL0 = 0;                                          // Turn off ADC subsystem
+            ADC10CTL1 = INCH_3 | ADC10DIV_3 ;                       // Channel 4, ADC10CLK/4
+            ADC10AE0 = BIT3;                                        // Make P1.3 analog input
+            ADC10CTL0 = SREF_0 | ADC10SHT_3 | ADC10ON | ENC;        // Vcc & Vss as reference
 
+            ADC10CTL0 |= ADC10SC;                                   // Start a conversion
+            while(ADC10CTL1 & ADC10BUSY);                           // Wait for conversion to complete
+            L_val[x] = ADC10MEM;                                   // collect that 10-bit value
+            if (L_val[x] > 0x0180) P1OUT |= BIT0;
+            else                   P1OUT &= ~BIT0;
 
+            x = (x+1) & 0xF;                            // This is a mod 16 increment
+} // end Left_Sensor
+
+void Center_Sensor() {
+    // CENTER
+    static unsigned short C_val[16];
+    static unsigned char y = 0;
+
+            ADC10CTL0 = 0;                                          // Turn off ADC subsystem
+            ADC10CTL1 = INCH_4 | ADC10DIV_3 ;                       // Channel 4, ADC10CLK/4
+            ADC10AE0 = BIT4;                                        // Make P1.4 analog input
+            ADC10CTL0 = SREF_0 | ADC10SHT_3 | ADC10ON | ENC;        // Vcc & Vss as reference
+
+            ADC10CTL0 |= ADC10SC;                                   // Start a conversion
+            while(ADC10CTL1 & ADC10BUSY);                           // Wait for conversion to complete
+
+            C_val[y] = ADC10MEM;                                   // collect that 10-bit value
+            if (C_val[y] > 0x0195) P1OUT |= (BIT0|BIT6);
+            else                   P1OUT &= ~(BIT0|BIT6);
+
+            y = (y+1) & 0xF;                            // This is a mod 16 increment
+} // end Center_Sensor
+
+void Right_Sensor() {
+    // RIGHT
+    static unsigned short R_val[16];
+    static unsigned char z = 0;
+
+            ADC10CTL0 = 0;                                          // Turn off ADC subsystem
+            ADC10CTL1 = INCH_5 | ADC10DIV_3 ;                       // Channel 4, ADC10CLK/4
+            ADC10AE0 = BIT5;                                        // Make P1.5 analog input
+            ADC10CTL0 = SREF_0 | ADC10SHT_3 | ADC10ON | ENC;        // Vcc & Vss as reference
+
+            ADC10CTL0 |= ADC10SC;                                   // Start a conversion
+            while(ADC10CTL1 & ADC10BUSY);                           // Wait for conversion to complete
+            R_val[z] = ADC10MEM;                                   // collect that 10-bit value
+            if (R_val[z] > 0x0230) P1OUT |= BIT6;
+            else                   P1OUT &= ~BIT6;
+
+            z = (z+1) & 0xF;                            // This is a mod 16 increment
+} // end Right_Sensor
